9eff385b00da76be9c00466f9772125cf5635b71
S@chin R@thore <115761551+Sachin20221301@users.noreply.github.com>
2023-06-24 20:24:13 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit_app",
+    version="0.1",
+    install_requires=[
+        "streamlit",
+        "ultralyticsplus"
+    ],
+    packages=setuptools.find_packages(),
+)
